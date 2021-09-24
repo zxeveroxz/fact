2a389df24ced6ff2036d5a6024619a28ffd5df2d
Sergio Zegarra <s.zegarra@hotmail.com>
2021-09-24 18:44:57 -0500

    first commit

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Path to the front controller (this file)
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+define('FCPATH', __DIR__.DIRECTORY_SEPARATOR);
 
 /*
  *---------------------------------------------------------------
@@ -11,21 +11,20 @@ define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
  * our autoloader, along with Composer's, loads our constants
  * and fires up an environment-specific bootstrapping.
  */
-
 // Ensure the current directory is pointing to the front controller's directory
 chdir(__DIR__);
 
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
-$pathsConfig = FCPATH . '../app/Config/Paths.php';
+$pathsConfig = FCPATH.'../app/Config/Paths.php';
 // ^^^ Change this if you move your application folder
 require realpath($pathsConfig) ?: $pathsConfig;
 
 $paths = new Config\Paths();
 
 // Location of the framework bootstrap file.
-$bootstrap = rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
-$app       = require realpath($bootstrap) ?: $bootstrap;
+$bootstrap = rtrim($paths->systemDirectory, '\\/ ').DIRECTORY_SEPARATOR.'bootstrap.php';
+$app = require realpath($bootstrap) ?: $bootstrap;
 
 /*
  *---------------------------------------------------------------
